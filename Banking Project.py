0b31bfd6970ef545c4dbc0fee27d409b83b66574
Abhiram Nair <nairabhiram48@gmail.com>
2024-04-03 17:50:15 -0500

    Added data to MySQL using python

--- Banking Project.py
+++ Banking Project.py
@@ -2,11 +2,15 @@ import mysql.connector
 connection = mysql.connector.connect(user = 'root', database = 'account_schema', password = 'Slimyship41$')
 
 sampleQuery = ('SELECT * FROM account_schema.Workers;')
+addData = ('INSERT INTO Workers VALUES (9, "Abhiram", 50000, "Books");')
 cursor = connection.cursor()
+cursor.execute(addData)
 cursor.execute(sampleQuery)
 for row in cursor:
     print(row)
 
+connection.commit()
+cursor.close()
 connection.close()
 
 name = input("Enter your Username: ")
