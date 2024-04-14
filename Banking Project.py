36e35ea9353580a22544aa87e3291df3546af2fb
Abhiram Nair <nairabhiram48@gmail.com>
2024-04-14 14:22:21 -0500

    Finished all back-end functions

--- Banking Project.py
+++ Banking Project.py
@@ -5,6 +5,7 @@ import sys
 connection = db_connection.get_connection()
 cursor = connection.cursor()
 
+
 name = input("Enter your Username: ")
 password = input("Enter your Password: ")
 
@@ -19,9 +20,52 @@ result2 = cursor.fetchone()
 if result1 and result2:
     print(f'''\nHello {name}!
 Welcome to Our Bank.''')
-    global userChoice
     userChoice = BankingProjectFunctions.intro()
-    
+
+    while True:
+        
+        if userChoice == 0:
+            
+            if name == "AdminUser" and password == "AdminPass":
+                adminChoice = input("You are an admin. Would you like to see the data (Yes/No)? ")
+                if adminChoice == "Yes":
+                    query = "SELECT * FROM account_schema.bankAccounts;"
+                    cursor.execute(query)
+                    for row in cursor:
+                        print(row)
+                else:
+                    print("OK")
+            else:
+                print("You are not an admin.")
+
+        elif userChoice == 1:
+            print(f'Your account balance is currently: {BankingProjectFunctions.getBalance(name)}')
+            
+        elif userChoice == 2:
+            BankingProjectFunctions.deposit(name)
+            
+        elif userChoice == 3:
+            BankingProjectFunctions.withdraw(name)
+
+        elif userChoice == 4:
+            BankingProjectFunctions.createAccount()
+
+        elif userChoice == 5:
+            BankingProjectFunctions.deleteAccount(name, password)
+            
+        elif userChoice == 6:
+            BankingProjectFunctions.changeCredentials(name)
+
+        elif userChoice == 7:
+            sys.exit("Thank you for using Our Bank. Come again soon!")
+            
+        else:
+            sys.exit("That is not a valid choice. \nThank you for using Our Bank. Come again soon!")
+            
+        userChoice = BankingProjectFunctions.intro()
+
+        connection.commit()
+
 else:
     newAccount = input("That account does not exist. Would you like to create a new account with us (Yes/No)? ")
     if newAccount == 'Yes':
@@ -30,39 +74,6 @@ else:
         sys.exit("Thank you for using This Bank. Hope you come again!")
     else:
         sys.exit('That is not a valid choice.')
-
-if userChoice == 0:
-    
-    if name == "AdminUser" and password == "AdminPass":
-        adminChoice = input("You are an admin. Would you like to see the data (Yes/No)? ")
-        if adminChoice == "Yes":
-            query = "SELECT * FROM account_schema.bankAccounts;"
-            cursor.execute(query)
-            for row in cursor:
-                print(row)
-        else:
-            print("OK")
-            BankingProjectFunctions.intro()
-    else:
-        print("You are not an admin.")
-        BankingProjectFunctions.intro()
-
-elif userChoice == 1:
-    print(f'Your account balance is currently: {BankingProjectFunctions.getBalance(name)}')
-    BankingProjectFunctions.intro()
-
-elif userChoice == 4:
-    BankingProjectFunctions.createAccount()
-    BankingProjectFunctions.intro()
-    
-elif userChoice == 5:
-    BankingProjectFunctions.deleteAccount(name, password)
-    BankingProjectFunctions.intro()
-    
-elif userChoice == 7:
-    sys.exit("Thank you for using Our Bank. Come again soon!")
-
-
-connection.commit()
+        
 cursor.close()
 connection.close()
--- BankingProjectFunctions.py
+++ BankingProjectFunctions.py
@@ -1,9 +1,10 @@
 import db_connection
+import sys
 
 connection = db_connection.get_connection()
 cursor = connection.cursor()
 
-def intro():    
+def intro():
     global userChoice
     userChoice = int(input('''\nWhat would you like to do today?
                                                     
@@ -28,6 +29,41 @@ def getBalance(name):
     else:
         return None
 
+def deposit(name):
+    while True:
+        try:
+            deposit_amount = float(input("Enter amount: "))
+            if deposit_amount > 0:
+                break
+            else:
+                print("Invalid amount. Please enter a positive number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    sql_query = "UPDATE bankAccounts SET Balance = Balance + %s WHERE UserName = %s"
+    cursor.execute(sql_query, (deposit_amount, name))
+    print(f"Successfully deposited {deposit_amount}.")
+    
+    connection.commit()
+    
+def withdraw(name):
+    while True:
+        try:
+            withdraw_amount = float(input("Enter amount: "))
+            if withdraw_amount > 0:
+                break
+            else:
+                print("Invalid amount. Please enter a positive number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    sql_query = "UPDATE bankAccounts SET Balance = Balance - %s WHERE UserName = %s"
+    cursor.execute(sql_query, (withdraw_amount, name))
+    print(f"Successfully withdrew {withdraw_amount}.")
+
+    
+    connection.commit()
+    
 def createAccount():
 
     newAccountUser= input("Enter Account Username: ")
@@ -37,14 +73,31 @@ def createAccount():
     cursor.execute(query, (newAccountUser, newAccountPass))
     
     connection.commit()
-    cursor.close()
-    connection.close()
+
     
 def deleteAccount(name, password):
     if input('Are you sure you want to do this?\n Type "YES" to delete account: ') == "YES":
       query = "DELETE FROM bankAccounts WHERE UserName = %s AND Password = %s"
       cursor.execute(query, (name, password))
+      sys.exit("Your account has been deleted. Goodbye!")
+      
+    else:
+        print("Ok, your account will remain active.")
       
     connection.commit()
-    cursor.close()
-    connection.close()
+    
+def changeCredentials(name):
+    
+    new_username = input("Enter new username (Enter current username if you do not want to change it): ")
+    new_password = input("Enter new password (Enter current password if you do not want to change it): ")
+
+    query = "UPDATE bankAccounts SET UserName = %s WHERE UserName = %s"
+    cursor.execute(query, (new_username, name))
+    print(f"Successfully changed username to {new_username}.")
+
+
+    query = "UPDATE bankAccounts SET Password = %s WHERE UserName = %s"
+    cursor.execute(query, (new_password, name))
+    print(f"Successfully changed password for {name}.")
+
+    connection.commit()   
